6b2aa9f85ba563594e7512acbbba89534e8e2543
Maarten van den Hooven <maartenv@kontent.ai>
2025-05-28 12:11:20 +0200

    service listing

--- src/pages/BlogPage.tsx
+++ src/pages/BlogPage.tsx
@@ -10,9 +10,11 @@ import { defaultPortableRichTextResolvers, isEmptyRichText } from "../utils/rich
 import { PortableText } from "@portabletext/react";
 import { transformToPortableText } from "@kontent-ai/rich-text-resolver";
 import { LanguageCodenames } from "../model";
-import { IUpdateMessageData, applyUpdateOnItemAndLoadLinkedItems } from "@kontent-ai/smart-link";
-import { useLivePreview } from "../context/SmartLinkContext";
+import { IRefreshMessageData, IRefreshMessageMetadata, IUpdateMessageData, applyUpdateOnItemAndLoadLinkedItems } from "@kontent-ai/smart-link";
+import { useCustomRefresh, useLivePreview } from "../context/SmartLinkContext";
 import { createElementSmartLink, createItemSmartLink } from "../utils/smartlink";
+import { useSuspenseQueries } from "@tanstack/react-query";
+import { Replace } from "../utils/types";
 
 const useBlogPage = (isPreview: boolean, lang: string | null) => {
   const { environmentId, apiKey } = useAppContext();
@@ -115,12 +117,49 @@ const useBlogPosts = (isPreview: boolean, lang: string | null) => {
 };
 
 const BlogPage: React.FC = () => {
+  const { environmentId, apiKey } = useAppContext();
   const [searchParams] = useSearchParams();
   const isPreview = searchParams.get("preview") === "true";
   const lang = searchParams.get("lang");
 
   const blogPage = useBlogPage(isPreview, lang);
   const blogPosts = useBlogPosts(isPreview, lang);
+
+  const [blogPageData] = useSuspenseQueries({
+    queries: [
+      {
+        queryKey: ["landing_page"],
+        queryFn: () =>
+          createClient(environmentId, apiKey, isPreview)
+            .items()
+            .type("landing_page")
+            .limitParameter(1)
+            .toPromise()
+            .then(res =>
+              res.data.items[0] as Replace<Page, { elements: Partial<Page["elements"]> }> ?? null
+            )
+            .catch((err) => {
+              if (err instanceof DeliveryError) {
+                return null;
+              }
+              throw err;
+            }),
+      },
+    ],
+  });
+
+  const onRefresh = useCallback(
+    (_: IRefreshMessageData, metadata: IRefreshMessageMetadata, originalRefresh: () => void) => {
+      if (metadata.manualRefresh) {
+        originalRefresh();
+      } else {
+        blogPageData.refetch();
+      }
+    },
+    [blogPage],
+  );
+
+  useCustomRefresh(onRefresh);
 
   if (!blogPage || !blogPosts) {
     return <div className="flex-grow" />;
